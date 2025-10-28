a74637dcc8623d0f594cf3bcca5f4e249b686b69
가동재 <masterforce999@naver.com>
2025-10-28 21:56:57 +0900

    업데이트

--- src/page/kr/page.tsx
+++ src/page/kr/page.tsx
@@ -16,7 +16,14 @@ export default function Page() {
         <MapPage />
       </div>
       <div className="w-screen h-screen bg-gray-200 px-8 py-16">
-        <h2 className="heading02b mb-8">참여인 또는 컨텐츠 고민</h2>
+        <h2 className="heading02b mb-8">후원사</h2>
+      </div>
+      <div
+        className="w-screen h-screen bg-white px-8 py-16 bg-fixed bg-center bg-cover relative"
+        style={{ backgroundImage: "url('/main/main05.jpeg')" }}
+      >
+        <h2 className="heading02b mb-8 text-white relative z-50">묵주기도</h2>
+        <div className="bg-backdrop" />
       </div>
     </main>
   );
